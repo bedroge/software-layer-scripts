c9481894977e0fffd78c61faa34c089951df8c24
Bob Dröge <b.e.droge@rug.nl>
2026-09-13 14:24:39 +0200

    8 cores for rocBLAS

--- eb_hooks.py
+++ eb_hooks.py
@@ -2498,7 +2498,7 @@ PARALLELISM_LIMITS = {
         CPU_TARGET_A64FX: (set_maximum, 6),
     },
     'rocBLAS': {
-        '*': (set_maximum, 1),
+        '*': (set_maximum, 8),
     },
     'TensorFlow': {
         '*': (divide_by_factor, 2),
